03ae84550d5370a8ec5a3935032196a4db13b37e
rcabezas29 <raul.29cg@gmail.com>
2023-05-29 18:42:42 +0200

    Fixed data science funcions

--- describe.py
+++ describe.py
@@ -1,65 +1,61 @@
 import sys
 import pandas as pd
 import math
+import numpy as np
 
 def	data_third_quartile(data) -> float:
-	return data[(data_count(data) / 4) * 3]
+	sorted_data = np.sort(data)
+	return sorted_data[int(data_count(data) / 4) * 3]
 
-def	data_second_quartile(data) -> float:
-	return data[data_count(data) / 2]
+def	data_median(data) -> float:
+	sorted_data = np.sort(data)
+	return sorted_data[int(data_count(data) / 2)]
 
 def	data_first_quartile(data) -> float:
-	return data[data_count(data) / 4]
+	sorted_data = np.sort(data)
+	return sorted_data[int(data_count(data) / 4)]
 
 def data_max(data) -> float:
-    max = sys.float_info.min
-    for val in data:
-        if (val > max):
-            max = val
-    return max
+	max = sys.float_info.min
+	for val in data:
+		if (val > max and not math.isnan(val)):
+			max = val
+	return max
 
 def data_min(data) -> float:
-    min = sys.float_info.max
-    for val in data:
-        if (val < min):
-            min = val
-    return min
+	min = sys.float_info.max
+	for val in data:
+		if (val < min and not math.isnan(val)):
+			min = val
+	return min
 
 def data_standard_deviation(data) -> float:
-    mean = data_mean(data)
-    sum = 0
-    for val in data:
-        sum += (val - mean)**2
-    return math.sqrt(sum / (data_count - 1))
+	mean = data_mean(data)
+	size = data_count(data)
+	sum = 0
+	for val in data:
+		if math.isnan(val):
+			val = 0
+		sum += (val - mean)**2
+	return math.sqrt(sum / (size - 1))
 
 def data_mean(data) -> float:
-    sum = 0
-    for val in data:
-        sum += val
-    return sum / data_count(data)
+	sum = 0
+	size = data_count(data)
+	for val in data:
+		if math.isnan(val):
+			continue
+		else:
+			sum += val
+	return sum / data_count(data)
 
 def data_count(data) -> int:
-    i = 0
-    for _ in data:
-        i += 1
-    return i
+	i = 0
+	for _ in data:
+		i += 1
+	return i
 
 if __name__ == "__main__":
-    try:
-        data = pd.read_csv(sys.argv[1])
-        f1 = data['Arithmancy']
-        f2 = data['Astronomy']
-        f3 = data['Herbology']
-        f4 = data['Defense Against the Dark Arts']
-        f5 = data['Divination']
-        f6 = data['Muggle Studies,Ancient Runes']
-        f7 = data['History of Magic']
-        f8 = data['Transfiguration']
-        f9 = data['Potions']
-        f10 = data['Care of Magical Creatures']
-        f11 = data['Charms']
-        f12 = data['Flying']
-        
-    except:
-        print('Unexpected argument')
-        exit(1)
+	data = pd.read_csv(sys.argv[1])
+	f1 = data['Arithmancy']
+	print(data_third_quartile(f1))

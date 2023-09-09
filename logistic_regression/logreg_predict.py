2b52216cd105a6f2df1f706a8a0eb895497bb953
rcabezas29 <raul.29cg@gmail.com>
2023-09-09 23:33:27 +0200

    Write prediction to file

--- logistic_regression/logreg_predict.py
+++ logistic_regression/logreg_predict.py
@@ -16,6 +16,26 @@ def	usage():
 def	clean_data(df: pd.DataFrame) -> pd.DataFrame:
 	return df[['Astronomy','Herbology','Defense Against the Dark Arts','Divination','Muggle Studies','Ancient Runes','Transfiguration','Charms','Flying']]
 
+def	write_prediction_file(house_prediction):
+	with open('houses.csv', mode='w', newline='') as file:
+		writer = csv.writer(file)
+		writer.writerow(['Index', 'Hogwarts House'])
+		for index, value in enumerate(house_prediction):
+			writer.writerow([index, value])
+
+def	execute_prediction(x, houses_thetas) -> list:
+	results = pd.DataFrame()
+	for h in houses:
+		lr = logReg(houses_thetas[h].to_numpy())
+		x = lr.normalize_data(x)
+		results[h] = [i * 100 for i in lr.predict(x)]
+
+	house_prediction = []
+	for _, row in results.iterrows():
+		house_prediction.append(row.idxmax())
+
+	return house_prediction
+
 def	main():
 	try:
 		df = pd.read_csv(sys.argv[1], index_col=0)
@@ -27,17 +47,9 @@ def	main():
 	x = clean_data(df[features].fillna(0)).to_numpy()
 	houses_thetas = pd.read_csv(".weights.csv")
 
-	results = pd.DataFrame()
+	house_prediction = execute_prediction(x, houses_thetas)
 
-	for h in houses:
-		lr = logReg(houses_thetas[h].to_numpy())
-		x = lr.normalize_data(x)
-		results[h] = [i * 100 for i in lr.predict(x)]
-
-	house_prediction = []
-	for _, row in results.iterrows():
-		house_prediction.append(row.idxmax())
-
+	write_prediction_file(house_prediction)
 
 if __name__ == "__main__":
     main()
